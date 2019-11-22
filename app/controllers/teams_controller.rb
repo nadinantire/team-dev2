4ab8b1e392313c80fb3c4714143928d3eff94c0c
nadinantire <nadinantire@gmail.com>
2019-11-22 18:34:30 +0200

    commitments

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,76 @@
+class TeamsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_team, only: %i[show edit update destroy]
+
+  def index
+    @teams = Team.all
+  end
+
+  def show
+    if params[:leader]
+    @team.owner=nil
+    @user=User.find(params[:id])
+    @team.owner=@user
+    @team.update(team_params)
+    else
+    @working_team = @team
+    change_keep_team(current_user, @team)
+    end
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def edit; end
+
+  def create
+    @team = Team.new(team_params)
+    @team.owner = current_user
+    if @team.save
+      @team.invite_member(@team.owner)
+      redirect_to @team, notice: I18n.t('views.messages.create_team')
+    else
+      flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
+      render :new
+    end
+  end
+
+ def transfer_authority
+ 
+end
+
+  def update
+    if params[:owner_id]
+      @team.update(owner_id: params[:owner_id])
+      @team.assigns.each do |assign|  
+        AssignMailer.assign_mail(assign.user.email,assign.user.password).deliver
+        end
+      redirect_to @team, notice: 'leader changed'
+    elsif @team.update(team_params)
+        redirect_to @team, notice: I18n.t('views.messages.update_team')
+      else
+        flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
+        render :edit
+      end
+  end
+
+  def destroy
+    @team.destroy
+    redirect_to teams_url, notice: I18n.t('views.messages.delete_team')
+  end
+
+  def dashboard
+    @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
+  end
+
+  private
+
+  def set_team
+    @team = Team.friendly.find(params[:id])
+  end
+
+  def team_params
+    params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id leader]
+  end
+end
---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < ApplicationMailer
+    def contact_mail(agenda)
+        @agenda=agenda
+        mail to: "example@gmail.com",  suject: "Agenda deleted"
+
+    end
+
+   
+
+end
